041f9fa7e098a8e90578417665b24cc6415ae67e
sakoya00 <65825897+sakoya00@users.noreply.github.com>
2020-10-04 23:55:15 -0500

    Update timezone_clean.R

--- timezone_clean.R
+++ timezone_clean.R
@@ -1,8 +1,11 @@
 #setwd ("D:/ssda job")
 library(tidyverse)
 
-ssda <- read.csv("ssda_tz_0.csv", header= T)
+ssda <- read.csv("ssda_tz_0.csv", header= T, na.strings=c(""," ","NA"))
 ssda_tib <- as_tibble(ssda)
 ssda_tib <- select(ssda_tib, followers_count, last_tweeted_at_GMT)
 ssda_tib <- ssda_tib %>%
-mutate(last_tweeted_at_GMT= substr(last_tweeted_at_GMT,12,19))
+mutate(last_tweeted_at_GMT= substr(last_tweeted_at_GMT,12,19)) %>%
+mutate(last_tweeted_at_GMT= as.numeric(gsub(":", "", ssda_tib$last_tweeted_at_GMT)))
+ssda_tib <- na.omit(ssda_tib)
+       
