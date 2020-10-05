3c96923f03b30002a163df6855df88dfe8071e69
sakoya00 <65825897+sakoya00@users.noreply.github.com>
2020-10-05 11:15:42 -0500

    Update timezone_clean.R

--- timezone_clean.R
+++ timezone_clean.R
@@ -8,9 +8,9 @@ ssda_tib <- as_tibble(ssda)
 ssda_tib <- select(ssda_tib, followers_count, last_tweeted_at_GMT)
 sum_followers <- sum(ssda_tib$followers_count)
 ssda_tib <- ssda_tib %>%
-mutate(last_tweeted_at_GMT= substr(last_tweeted_at_GMT,12,19)) %>%
-mutate(last_tweeted_at_GMT= as.numeric(gsub(":", "", ssda_tib$last_tweeted_at_GMT))) %>%
-mutate(fraction_of_followers= ssda_tib$followers_count/sum_followers)
+            mutate(last_tweeted_at_GMT= substr(last_tweeted_at_GMT,12,19)) %>%
+            mutate(last_tweeted_at_GMT= as.numeric(gsub(":", "", ssda_tib$last_tweeted_at_GMT))) %>%
+            mutate(fraction_of_followers= ssda_tib$followers_count/sum_followers)
 ssda_tib <- na.omit(ssda_tib)
 ssda_tib$bin= cut(ssda_tib$last_tweeted_at_GMT, c(000000, 010000,020000,030000,040000,050000,
                                                   060000,070000,080000,090000,100000,110000,
@@ -18,12 +18,10 @@ ssda_tib$bin= cut(ssda_tib$last_tweeted_at_GMT, c(000000, 010000,020000,030000,0
                                                   180000, 190000, 200000, 210000, 220000, 230000, 
                                                   240000))       
 
-ssda_tib %>% 
-    group_by(bin) %>% 
-    mutate(N=n()) %>%
-    ungroup() %>% 
-    filter(N==max(N))
-
-ssda_tib %>%
-    group_by(bin) %>%
-    summarise(test_sum=sum(fraction_of_followers))
+ssda_tib <- ssda_tib %>%
+            group_by(bin) %>%
+            mutate(N=n())%>%
+            mutate(weight_by_followers=sum(fraction_of_followers))%>%
+            summarise(adjusted_weight=N*weight_by_followers)
+write.csv(ssda_tib, "ssda_weight.csv")
+      
