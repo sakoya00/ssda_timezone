90ab5deb5cbb050ca07fdad0a14b7f6d4afb51c6
sakoya00 <65825897+sakoya00@users.noreply.github.com>
2020-10-04 22:52:01 -0500

    Create timezone_clean.R

---
+++ timezone_clean.R
@@ -0,0 +1,6 @@
+#setwd ("D:/ssda job")
+library(tidyverse)
+
+ssda <- read.csv("ssda_tz_0.csv", header= T)
+ssda_tib <- as_tibble(ssda)
+ssda_tib <- select(ssda_tib, followers_count, last_tweeted_at_GMT)
