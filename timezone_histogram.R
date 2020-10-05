2f8147e1924b2884b47395c0203e114d415edff1
sakoya00 <65825897+sakoya00@users.noreply.github.com>
2020-10-05 13:35:35 -0500

    Delete timezone_histogram.R

--- timezone_histogram.R
+++
@@ -1,7 +0,0 @@
-#Histogram visualization after cleaning data with R and Excel
-ssda2 <- read.csv("ssda_weight.csv")
-
-ssda2 <- ssda2 %>%
-         ggplot (aes(x=bin, y= adjusted_weight))+
-         geom_point()+
-         scale_x_continuous(breaks = scales::pretty_breaks(n =23))
