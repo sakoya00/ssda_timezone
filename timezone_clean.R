bb212b712d6221349a1e56a390de4fb774a86362
sakoya00 <65825897+sakoya00@users.noreply.github.com>
2020-10-04 23:05:27 -0500

    Update timezone_clean.R

--- timezone_clean.R
+++ timezone_clean.R
@@ -4,3 +4,5 @@ library(tidyverse)
 ssda <- read.csv("ssda_tz_0.csv", header= T)
 ssda_tib <- as_tibble(ssda)
 ssda_tib <- select(ssda_tib, followers_count, last_tweeted_at_GMT)
+
+substr(test_str,12,19)
